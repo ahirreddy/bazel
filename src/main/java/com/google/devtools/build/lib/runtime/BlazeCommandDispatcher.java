30b4f9ca53b2ba053479010daefcaa2a4c1e7c69
adonovan <adonovan@google.com>
2020-06-10 06:22:23 -0700

    bazel: make Starlark profiler work in Bazel
    Fixes https://github.com/bazelbuild/bazel/issues/10890
    
    Details:
    - configure copybara so that Bazel sees JNI.java.oss as JNI.java.
    - use the correct .so library name for Bazel (no "-jni").
    - add the dynamic library to to Bazel's install-base using
      the platform's required name (libx.so, libx.dylib, x.dll).
    - report errors during command startup, don't just store them. :)
    - don't consider SIG_DFL as "signal already in use". The pthreads
      package used by Bazel sets SIGPROF to DFL whereas Blaze's uses IGN.
    
    RELNOTES:
    The --starlark_cpu_profile=<file> flag writes a profile in
    pprof format containing a statistical summary of CPU usage
    by all Starlark execution during the bazel command. Use it
    to identify slow Starlark functions in loading and analysis.
    PiperOrigin-RevId: 315678587

--- src/main/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcher.java
+++ src/main/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcher.java
@@ -350,7 +350,7 @@ public class BlazeCommandDispatcher implements CommandDispatcher {
         out = new FileOutputStream(commonOptions.starlarkCpuProfile);
       } catch (IOException ex) {
         String message = "Starlark CPU profiler: " + ex.getMessage();
-        storedEventHandler.handle(Event.error(message));
+        outErr.printErrLn(message);
         return createDetailedCommandResult(
             message,
             ExitCode.LOCAL_ENVIRONMENTAL_ERROR,
@@ -360,7 +360,7 @@ public class BlazeCommandDispatcher implements CommandDispatcher {
         Starlark.startCpuProfile(out, Duration.ofMillis(10));
       } catch (IllegalStateException ex) { // e.g. SIGPROF in use
         String message = Strings.nullToEmpty(ex.getMessage());
-        storedEventHandler.handle(Event.error(message));
+        outErr.printErrLn(message);
         return createDetailedCommandResult(
             message,
             ExitCode.LOCAL_ENVIRONMENTAL_ERROR,
